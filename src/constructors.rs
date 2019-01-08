c7c6b2f7da67c497ef6f59fa4ea65bf9dc808a07
Afnan Enayet <afnan@afnan.io>
2019-01-07 21:23:28 -0500

    Move unit tests back to the constructors file

--- src/constructors.rs
+++ src/constructors.rs
@@ -117,3 +117,72 @@ impl OAConstructor for Bose {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ndarray::arr2;
+    #[test]
+    // Initialize with a non prime
+    fn bose_non_prime() {
+        let bose = Bose {
+            prime_base: 4,
+            dimensions: 3,
+        };
+        assert!(bose.gen().is_err());
+    }
+
+    #[test]
+    // Initialize the Bose constructor with bad `dimensions` values
+    fn bose_bad_dims() {
+        let bose = Bose {
+            prime_base: 5,
+            dimensions: 1,
+        };
+        assert!(bose.gen().is_err());
+
+        let bose = Bose {
+            prime_base: 5,
+            dimensions: 7,
+        };
+        assert!(bose.gen().is_err());
+
+        let bose = Bose {
+            prime_base: 13,
+            dimensions: 20,
+        };
+        assert!(bose.gen().is_err());
+    }
+
+    #[test]
+    fn bose_init_2() {
+        let bose = Bose {
+            prime_base: 2,
+            dimensions: 2,
+        };
+        let oa = bose.gen().unwrap();
+        let ground_truth = arr2(&[[0, 0], [0, 1], [1, 0], [1, 1]]);
+        assert!(oa.points == ground_truth);
+    }
+
+    #[test]
+    fn bose_init_3() {
+        let bose = Bose {
+            prime_base: 3,
+            dimensions: 3,
+        };
+        let oa = bose.gen().unwrap();
+        let ground_truth = arr2(&[
+            [0, 0, 0],
+            [0, 1, 1],
+            [0, 2, 2],
+            [1, 0, 0],
+            [1, 1, 1],
+            [1, 2, 2],
+            [2, 0, 0],
+            [2, 1, 1],
+            [2, 2, 2],
+        ]);
+        assert!(oa.points == ground_truth);
+    }
+}
--- tests/bose_tests.rs
+++ tests/bose_tests.rs
@@ -1,68 +1,13 @@
-use ndarray::arr2;
 /// Unit tests for the Bose array constructor
 use oars::constructors::Bose;
-use oars::oa::OAConstructor;
+use oars::oa::{verify_oa, OAConstructor};
 
 #[test]
-// Initialize with a non prime
-fn bose_non_prime() {
-    let bose = Bose {
-        prime_base: 4,
-        dimensions: 3,
-    };
-    assert!(bose.gen().is_err());
-}
-
-#[test]
-// Initialize the Bose constructor with bad `dimensions` values
-fn bose_bad_dims() {
-    let bose = Bose {
-        prime_base: 5,
-        dimensions: 1,
-    };
-    assert!(bose.gen().is_err());
-
-    let bose = Bose {
-        prime_base: 5,
-        dimensions: 7,
-    };
-    assert!(bose.gen().is_err());
-
-    let bose = Bose {
-        prime_base: 13,
-        dimensions: 20,
-    };
-    assert!(bose.gen().is_err());
-}
-
-#[test]
-fn bose_init_2() {
-    let bose = Bose {
-        prime_base: 2,
-        dimensions: 2,
-    };
-    let oa = bose.gen().unwrap();
-    let ground_truth = arr2(&[[0, 0], [0, 1], [1, 0], [1, 1]]);
-    assert!(oa.points == ground_truth);
-}
-
-#[test]
-fn bose_init_3() {
+fn test_bose_init_verify() {
     let bose = Bose {
         prime_base: 3,
         dimensions: 3,
     };
     let oa = bose.gen().unwrap();
-    let ground_truth = arr2(&[
-        [0, 0, 0],
-        [0, 1, 1],
-        [0, 2, 2],
-        [1, 0, 0],
-        [1, 1, 1],
-        [1, 2, 2],
-        [2, 0, 0],
-        [2, 1, 1],
-        [2, 2, 2],
-    ]);
-    assert!(oa.points == ground_truth);
+    assert!(verify_oa(&oa));
 }
