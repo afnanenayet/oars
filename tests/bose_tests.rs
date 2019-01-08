1e7e1d522f397ffc4ff8aa26a57b0627b07fb46c
Afnan Enayet <afnan@afnan.io>
2019-01-08 17:37:16 -0500

    Update bose integration tests

--- tests/bose_tests.rs
+++ tests/bose_tests.rs
@@ -5,8 +5,15 @@ use oars::oa::{verify_oa, OAConstructor};
 #[test]
 fn test_bose_init_verify() {
     let bose = Bose {
+        prime_base: 2,
+        dimensions: 2,
+    };
+    let oa = bose.gen().unwrap();
+    assert!(verify_oa(&oa));
+
+    let bose = Bose {
         prime_base: 3,
-        dimensions: 3,
+        dimensions: 2,
     };
     let oa = bose.gen().unwrap();
     assert!(verify_oa(&oa));
