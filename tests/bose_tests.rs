7ca83043f96e192437c52eed3897643249c36e8b
Afnan Enayet <afnan@afnan.io>
2019-01-08 21:33:38 -0500

    Add normalization tests

--- tests/bose_tests.rs
+++ tests/bose_tests.rs
@@ -1,6 +1,6 @@
 /// Unit tests for the Bose array constructor
 use oars::constructors::Bose;
-use oars::oa::{verify_oa, OAConstructor};
+use oars::oa::{verify_oa, OAConstructor, normalize}; 
 
 #[test]
 fn test_bose_init_verify() {
@@ -17,4 +17,24 @@ fn test_bose_init_verify() {
     };
     let oa = bose.gen().unwrap();
     assert!(verify_oa(&oa));
+}
+
+#[test]
+fn test_bose_normalize() {
+    let bose = Bose {
+        prime_base: 2,
+        dimensions: 2,
+    };
+    let oa = bose.gen().unwrap();
+    normalize(&oa, 0.0, true);
+    normalize(&oa, 1.0, true);
+
+    let bose = Bose {
+        prime_base: 3,
+        dimensions: 2,
+    };
+    let oa = bose.gen().unwrap();
+    assert!(verify_oa(&oa));
+    normalize(&oa, 0.0, true);
+    normalize(&oa, 1.0, true);
 }
