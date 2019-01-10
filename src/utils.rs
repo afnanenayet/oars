4323acae397815d98513ca5e0ea0efb4fced2635
Afnan Enayet <afnan@afnan.io>
2019-01-09 22:03:48 -0500

    Fix base conversion, tests

--- src/utils.rs
+++ src/utils.rs
@@ -8,10 +8,10 @@ use std::vec::Vec;
 /// be zero-padded.
 pub fn to_base_fixed(num: u32, base: u32, degree: u32) -> Vec<u32> {
     // The number in a the new base
-    let mut new_base = vec![0; (degree + 1) as usize];
+    let mut new_base = vec![0; degree as usize];
     let mut new_num = num;
 
-    for i in 0..=degree {
+    for i in 0..degree {
         let i = i as usize;
         new_base[i] = new_num % base;
         new_num /= base;
@@ -38,28 +38,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_to_base() {
+    fn test_to_base_fixed() {
         let num = 5;
         let base = 2;
-        let mut res = to_base(num, base);
+        let mut res = to_base_fixed(num, base, 3);
         res.reverse();
         assert!(res == vec![1, 0, 1]);
 
         let num = 1;
         let base = 2;
-        let mut res = to_base(num, base);
+        let mut res = to_base_fixed(num, base, 3);
         res.reverse();
-        assert!(res == vec![1]);
+        assert!(res == vec![0, 0, 1]);
 
         let num = 9;
         let base = 2;
-        let mut res = to_base(num, base);
+        let mut res = to_base_fixed(num, base, 5);
         res.reverse();
-        assert!(res == vec![1, 0, 0, 1]);
+        assert!(res == vec![0, 1, 0, 0, 1]);
 
         let num = 7;
         let base = 3;
-        let mut res = to_base(num, base);
+        let mut res = to_base_fixed(num, base, 2);
         res.reverse();
         assert!(res == vec![2, 1]);
     }
