58653b5edd1bb36a6b4ca57c297379bf51eee9f0
Afnan Enayet <afnan@afnan.io>
2019-04-17 17:38:17 -0400

    Update script to not clean the build
    Cleaning the build is no longer necessary for clippy

--- lint.sh
+++ lint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # Run the clippy linter on the code with all warnings enabled
 # that don't emit false positives
-cargo clean && cargo clippy -- -W clippy::all
+cargo clippy -- -W clippy::all
