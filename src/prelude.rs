7b06c060888047ca8731b3cc2937cc715f981c8e
Afnan Enayet <afnan@afnan.io>
2019-06-21 21:36:32 -0400

    Update prelude with updated paths
    Update prelude to reflect how error types have been renamed and moved

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,9 +2,8 @@
 //!
 //! Note that this module does not export structs for specific constructors.
 
-pub use crate::oa::{OAConstructionError, OAConstructor, OAResult, OA};
+pub use crate::oa::{OAConstructor, OAResult, OA};
 pub use crate::soa::{SOAConstructionError, SOAConstructor, SOAResult, SOA};
-pub use crate::utils::{Float, Integer};
-
+pub use crate::utils::{Float, Integer, OarsError, OarsResult};
 #[cfg(feature = "parallel")]
-pub use create::oa::ParOAConstructor;
+pub use crate::oa::ParOAConstructor;
