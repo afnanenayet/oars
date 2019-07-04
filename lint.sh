b20f5701beb9e6c6e26c1de73c5105fffb0fda2f
Afnan Enayet <afnan@afnan.io>
2019-07-04 15:18:57 -0400

    Move oars into a separate folder

--- lint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-# Run the clippy linter on the code with all warnings enabled
-# that don't emit false positives
-cargo clippy -- -W clippy::all
--- src/prelude.rs
+++
@@ -1,9 +0,0 @@
-//! Exports the necessary traits, structs, and error types to work with oars.
-//!
-//! Note that this module does not export structs for specific constructors.
-
-#[cfg(feature = "parallel")]
-pub use crate::oa::ParOAConstructor;
-pub use crate::oa::{OAConstructor, OAResult, OA};
-pub use crate::soa::{SOAConstructionError, SOAConstructor, SOAResult, SOA};
-pub use crate::utils::{Float, Integer, OarsError, OarsResult};
