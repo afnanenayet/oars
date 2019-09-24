e1811911cb25d13f8aa8db2497f5af6164d2b171
Afnan Enayet <afnan@afnan.io>
2019-09-23 21:25:38 -0700

    Implement SOA construction for even strengths

--- oars/src/constructors/soa.rs
+++ oars/src/constructors/soa.rs
@@ -146,9 +146,9 @@ pub struct LiuLiu<'a, T: Integer> {
 
 impl<'a, T: Integer> SOAConstructor for LiuLiu<'a, T> {
     fn gen(&self) -> SOAResult {
-        let t = self.oa.strength;
-        let m = self.oa.factors;
-        let s = self.oa.levels;
+        let t = self.oa.strength.to_u32().unwrap();
+        let m = self.oa.factors.to_u32().unwrap();
+        let s = self.oa.levels.to_u32().unwrap();
 
         // Calculate `k` and `q` based on `m = kt + q` knowing that `q` must be less than t. We can
         // easily calculate this by doing a rounded integer division (m / t) and let q be the
@@ -158,26 +158,26 @@ impl<'a, T: Integer> SOAConstructor for LiuLiu<'a, T> {
 
         // There are some special provisions for when q is >= t / 2. If this is true, we will have
         // to add an extra column and an extra row to the $R_1$ matrix.
-        let extra_col = q < (t / T::from(2).unwrap());
+        let extra_col = q < (t / 2);
 
         // Create the V_1 matrix as described in Liu & Liu, p. 1716.
         let v_1 = Array2::from(
-            (0..(t.to_usize().unwrap()))
+            (0..(t.to_u32().unwrap()))
                 .map(|i| {
-                    let i = T::from(i).unwrap();
-                    [i, t - i - T::from(1).unwrap()]
+                    let i = i;
+                    [i, t - i - 1]
                 })
-                .collect::<Vec<[T; 2]>>(),
+                .collect::<Vec<[u32; 2]>>(),
         );
         // Liu, Liu describes r_1 as an m by 2k (or + 1) matrix based on q. Since everything is
         // already zeroed out except for the v_1 blocks, we don't have to worry about explicitly
-        // setting the zero matrices.
+        // setting the zero submatrices.
         let r_1_dims = if extra_col {
             (m.to_usize().unwrap(), 2 * k.to_usize().unwrap())
         } else {
             (m.to_usize().unwrap(), 2 * k.to_usize().unwrap() + 1)
         };
-        let mut r_1 = Array2::<T>::zeros(r_1_dims);
+        let mut r_1 = Array2::<u32>::zeros(r_1_dims);
 
         // Add the V_1 matrices to R_1
         for i in 0..k.to_usize().unwrap() {
@@ -194,7 +194,7 @@ impl<'a, T: Integer> SOAConstructor for LiuLiu<'a, T> {
 
         // create the $d$ vector if necessary and add it to r_1
         if extra_col {
-            let mut d = Vec::<T>::with_capacity(m.to_usize().unwrap());
+            let mut d = Array1::zeros(m.to_usize().unwrap());
 
             for i in 0..q.to_usize().unwrap() {
                 d[i] = pow(s, i);
@@ -204,11 +204,14 @@ impl<'a, T: Integer> SOAConstructor for LiuLiu<'a, T> {
                 let idx = d.len() - i;
                 d[idx] = pow(s, t.to_usize().unwrap() - i);
             }
-
-            // TODO(afnan) figure out the offset for the last half of non-zero numbers in the
-            // array, finish the other for loop
+            r_1.slice_mut(s![.., 2 * k.to_usize().unwrap()]).assign(&d);
         }
-        unimplemented!("Still in progress");
+        let points = self.oa.points.map(|x| x.to_u32().unwrap()) * r_1;
+        Ok(SOA {
+            points,
+            strength: self.oa.strength.to_u32().unwrap(),
+            base: self.oa.levels.to_u32().unwrap(),
+        })
     }
 }
 
